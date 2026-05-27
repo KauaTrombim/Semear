efc9216cdf120085fc44d9a779cfd3c078c0714d
KauaTrombim <kauabenjamint@gmail.com>
2026-05-26 22:32:42 -0300

    python intermediario 1

--- python.I/metro_to_feet.py
+++
@@ -1,9 +0,0 @@
-CONSTANTE_DE_CONVERSAO = 3.28084
-
-def metro_to_feet(metro):
-    return metro * CONSTANTE_DE_CONVERSAO
-
-metro = float(input("Digite o valor em metros: "))
-feet  = metro_to_feet(metro)
-
-print(f"O valor convertido é de {feet} pés.")
--- python.II/simplePrint.py
+++
@@ -1,4 +0,0 @@
-teto = int(input("Digite um número limite"))
-
-for i in range(teto):
-    print(i)
